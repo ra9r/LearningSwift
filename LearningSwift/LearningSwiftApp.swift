434d2a1803dd3d6379558b8942f2c23f4b1e4135
Rodney Aiglstorfer <r@a9r.me>
2022-09-12 13:10:42 +0800

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Rodney Aiglstorfer on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
